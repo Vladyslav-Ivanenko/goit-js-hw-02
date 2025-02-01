3f9b39a9a1d66ae7f3e18ecef1dfb8f76e7585cb
VladyslavIvanenko <v.ivanenko.optimus@gmail.com>
2025-02-01 14:14:47 +0200

    done1

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,20 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
-  return contentValue + paddingValue * 2 + borderValue * 2;
+function checkForSpam(message) {
+  const prohibitedWord = "spam";
+  const secondProhibitedWord = "sale";
+  const messageLower = message.toLowerCase();
+  if (
+    messageLower.includes(prohibitedWord) ||
+    messageLower.includes(secondProhibitedWord)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
